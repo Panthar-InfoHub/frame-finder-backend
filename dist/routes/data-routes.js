7000c54763d40c37859c68ca44a150c5ae03f821
Gagan-deepp <gagan270903@gmail.com>
2025-09-12 17:36:10 +0530

    Remvoed dist

--- dist/routes/data-routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { getFrameData } from "../controllers/data-controller.js";
-import { auth } from "../middlwares/auth.js";
-export const dataRouter = Router();
-dataRouter.get("/frame-data", [
-    auth
-], getFrameData);
